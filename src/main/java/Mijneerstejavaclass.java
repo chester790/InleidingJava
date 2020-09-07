ca61055aa0722c1eb4dfe4710e686d6466417024
chester790 <cbornhijm@roc-dev.com>
2020-09-07 12:50:58 +0200

    Mijn eerste Java Klasse

---
+++ src/main/java/Mijneerstejavaclass.java
@@ -0,0 +1,8 @@
+public class Mijneerstejavaclass {
+    public static class MijnEersteJavaKlasse {
+
+        public static void main(String[] args) {
+            System.out.println("Dit is mijn eerste java klasse!");
+        }
+    }
+}
